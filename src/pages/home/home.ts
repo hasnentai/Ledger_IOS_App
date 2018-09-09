f415f8990c9a30ffb2596f519bab89b367cbb391
Hasnen tai <hasnentai@Hasnens-MacBook.local>
2018-09-09 17:34:52 +0530

    UI DONE

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  names=[
+    {
+      name : "Hasnen",
+      amoutn: "1200",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Saumil",
+      amoutn: "2000",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Mayur",
+      amoutn: "200",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Juned",
+      amoutn: "1200",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Uddesh",
+      amoutn: "400",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Sumana",
+      amoutn: "900",
+      date:"12-Sept-18"
+    },
+    {
+      name : "Purnima",
+      amoutn: "300",
+      date:""
+    },
+    {
+      name : "Hasnen",
+      amoutn: "200",
+      date:""
+    },
+  ];
 
+  constructor(public navCtrl: NavController) {
   }
 
 }
